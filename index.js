e7cac8286d10fc119e9a9b9f3841795f5288686c
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-26 12:37:51 +0100

    Set up post route for zoos

--- index.js
+++ index.js
@@ -15,10 +15,23 @@ const db = knex({
 server.use(express.json());
 server.use(helmet());
 
+// ============== GET ROUTES ============= //
+
 server.get('/api/zoos', async (req, res) => {
   try {
     const zoos = await db('zoos');
     res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// ============== POST ROUTES ============= //
+server.post('/api/zoos', async (req, res) => {
+  try {
+    const newZoo = await db('zoos').insert({ name: req.body.name });
+    const zooObject = await db('zoos').where({ id: newZoo[0] });
+    res.status(200).json(zooObject[0]);
   } catch (error) {
     res.status(500).json(error);
   }
