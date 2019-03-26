9d84be7dc7a9cfaae1c63967eccce467d1bbe488
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-26 13:08:46 +0100

    Set up delete route for zoos

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ server.get('/api/zoos/:id', async (req, res) => {
 server.post('/api/zoos', async (req, res) => {
   try {
     if (req.body.name) {
-      const newAnimalInZoo = await db('zoos').insert({ name: req.body.name });
-      const newAnimalInZooObject = await db('zoos').where({ id: newAnimalInZoo[0] });
+      const newAnimalInZooArray = await db('zoos').insert({ name: req.body.name });
+      const newAnimalInZooObject = await db('zoos').where({ id: newAnimalInZooArray[0] });
       res.status(200).json(newAnimalInZooObject[0]);
     } else {
       res.status(400).json({ message: 'Please make sure that you are sending request in following format [POST] { name: nameOfAnimalInZoo }' });
@@ -53,6 +53,26 @@ server.post('/api/zoos', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// ============== DELETE ROUTES ============= //
+
+server.delete('/api/zoos/:id', async (req, res) => {
+  try {
+    const isDeletedAnimalTrue = await db('zoos')
+      .where({ id: req.params.id })
+      .del();
+    if (isDeletedAnimalTrue) {
+      res.status(200).json({ message: 'Animal with provided id was deleted' });
+    } else {
+      res.status(400).json({ message: 'Animal with provided id does not exist' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+knex('users')
+  .where({ id: 135 })
+  .update({ email: 'hi@example.com' });
 
 const port = 3300;
 server.listen(port, function() {
