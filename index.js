077fbc81132a693812930cf506665f73cb0e1c82
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-26 12:27:27 +0100

    Set up get all route for zoos

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ const db = knex({
 server.use(express.json());
 server.use(helmet());
 
+server.get('/api/zoos', async (req, res) => {
+  try {
+    const zoos = await db('zoos');
+    res.status(200).json(zoos);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
